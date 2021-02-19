1b46735a020837f243292e69efccbf27906e1b07
Pavel Darii <paveldarii@yahoo.com>
2021-02-19 15:20:20 -0500

    heroku test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-//require("./seeders/seed"); // or npm run seed
+require("./seeders/seed"); // or npm run seed
 
 const PORT = process.env.PORT || 8080;
 
