e5fa3467f8f4403cf6fc01eb6c64fc37a7ab41ce
Pavel Darii <paveldarii@yahoo.com>
2021-02-19 10:40:13 -0500

    little change

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,6 @@
 const db = require("../models");
 
 module.exports = function (app) {
-  // App.get to pull up info for the workouts page
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then((dbWorkout) => {
@@ -11,7 +10,6 @@ module.exports = function (app) {
         res.status(400).json(err);
       });
   });
-  // App.get to pull up info for the range page
   app.get("/api/workouts/range", ({}, res) => {
     db.Workout.find({})
       .then((dbWorkout) => {
@@ -21,7 +19,6 @@ module.exports = function (app) {
         res.status(400).json(err);
       });
   });
-  // App.post to submit new completed workouts
   app.post("/api/workouts/", (req, res) => {
     db.Workout.create(req.body)
       .then((dbWorkout) => {
@@ -31,7 +28,6 @@ module.exports = function (app) {
         res.status(400).json(err);
       });
   });
-  // App.put to update workouts by MongoDB _id value and update the exercsise body
   app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate(
       { _id: req.params.id },
